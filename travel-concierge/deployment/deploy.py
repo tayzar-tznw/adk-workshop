c8ad236ed2c74aec3cbe0146a5ae53833e52b904
Tayzar <tayzar.tznw@gmail.com>
2025-05-28 13:04:42 +0900

    Added MCP tool and fix issues with colab

--- travel-concierge/deployment/deploy.py
+++ travel-concierge/deployment/deploy.py
@@ -54,8 +54,8 @@ def create(env_vars: dict[str, str]) -> None:
     remote_agent = agent_engines.create(
         app,
         requirements=[
-            "google-adk (>=0.0.2)",
-            "google-cloud-aiplatform[agent_engines] @ git+https://github.com/googleapis/python-aiplatform.git@copybara_738852226",
+            "google-adk (==0.5.0)",
+            "google-cloud-aiplatform[agent_engines] (==1.94.0)",
             "google-genai (>=1.5.0,<2.0.0)",
             "pydantic (>=2.10.6,<3.0.0)",
             "absl-py (>=2.2.1,<3.0.0)",
